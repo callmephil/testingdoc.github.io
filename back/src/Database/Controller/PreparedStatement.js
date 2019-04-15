e7053e5936c589c23dfbd2dcc86e6fe40b9aaf0c
Callmephil <phil19-94@hotmail.fr>
2019-04-15 17:14:15 +0300

    Back/Database/PreparedStatment : Part 3 @Previous Todo + Cleanup

--- back/src/Database/Controller/PreparedStatement.js
+++ back/src/Database/Controller/PreparedStatement.js
@@ -1,36 +1,48 @@
 const queryList = [
   // Users
   {
-    CREATE_USER: `INSERT INTO user_account (firstname, lastname, email, phoneNumber, auth0_sub) VALUES 
-        ($firstname, $lastname, $email, $phoneNumber, $auth0_sub)`,
-    UPDATE_USER: `UPDATE user_account SET firstname = $firstname, lastname = $lastname, email = $email, phoneNumber = $phoneNumber WHERE user_id = ?`,
-    BAN_USER: `UPDATE user_account SET disabled = 1 WHERE user_id = ?`,
-    GET_USER: `SELECT * FROM user_account WHERE user_id = ?`,
-    GET_USER_LIST: `SELECT * FROM user_account`,
+    USER_INS: `INSERT INTO user_account (firstname, lastname, email, phoneNumber, auth0_sub) VALUES 
+      ($firstname, $lastname, $email, $phoneNumber, $auth0_sub)`,
+    USER_UPD: `UPDATE user_account SET firstname = $firstname, lastname = $lastname, email = $email, phoneNumber = $phoneNumber WHERE user_id = ?`,
+    USER_BAN: `UPDATE user_account SET disabled = 1 WHERE user_id = ?`,
+    USER_SEL: `SELECT * FROM user_account WHERE user_id = ?`,
+    USER_SEL_ALL: `SELECT * FROM user_account`,
 
-    USER_LINKS_SEL: `SELECT * FROM user_links WHERE user_id = ?`,
     USER_LINKS_INS: `INSERT INTO user_links (user_id, link_type, link, last_update) VALUES (
-        $user_id, $link_type, $link, $last_update)`,
+      $user_id, $link_type, $link, $last_update)`,
+    USER_LINKS_SEL: `SELECT * FROM user_links WHERE user_id = ?`,
     USER_LINKS_DEL: `DELETE FROM user_links WHERE user_id = ? and link = ?`,
     USER_LINKS_UPD: `UPDATE user_links SET link = $link WHERE user_id = ? and rowId = ?`,
 
-    // Streak, Max_Streak ToMove
-    ATTENDANCE_INS: `INSERT INTO user_attendance (user_id, date, status, reason, streak, max_streak, key_amount) VALUES
-        ($user_id, $date, $status, $reason, $streak, $max_streak, $key_amount)`,
-    //
     USER_SKILLS_INS: `INSERT INTO user_skills (user_id, skill_id, value) VALUES ($user_id, $skill_id, $value)`,
     USER_SKILLS_UPD: `UPDATE user_skills SET value = $value WHERE user_id = $user_id and skill_id = $skill_id`,
-    USER_SKILLS_SEL: `SELECT * FROM user_skills WHERE user_id = ?`
+    USER_SKILLS_SEL: `SELECT * FROM user_skills WHERE user_id = ?`,
+
+    USER_COMPETENCIES_INS: `INSERT INTO user_competencies (user_id, competencie_id, value) VALUES ($user_id, $competencie_id, $value)`,
+    USER_COMPETENCIES_UPD: `UPDATE user_competencies SET value = $value WHERE user_id = ? AND competencie_id = ?`,
+
+    USER_NOTES_INS:`INSERT INTO user_notes (note_id, mentor_id, user_id, type, id, comment)`,
+    USER_NOTES_UPD:`UPDATE user_notes SET type = $type, id = $id, comment = $comment WHERE note_id = ?` ,
+    USER_NOTES_DEL:`DELETE FROM user_notes WHERE note_id = ?`,
+    USER_NOTES_SEL:`SELECT * FROM user_notes WHERE user_id = ?`,
+    USER_NOTES_SEL_ALL:`SELECT * FROM user_notes`,
     // USER_ASSIGNMENT_INS: `INSERT INTO user_assignment `, TODO
   },
+  // ATTENDANCES
+  {
+    ATTENDANCE_INS: `INSERT INTO user_attendance (user_id, date, status, reason, key_amount) VALUES
+      ($user_id, $date, $status, $reason, $key_amount)`,
+    ATTENDANCE_STREAK_UPD: `UPDATE user_attendance_streak SET streak = $streak, max_streak = $max_streak WHERE user_id = ?`,
+  },
+  // ASSIGNMENTS
   {
     ASSIGNMENT_INS: `INSERT INTO assignment (mentor_id, task_id, link, message, startDate, endDate) VALUES 
       ($mentor_id, $task_id, $link, $message, $startDate, $endDate)`,
     ASSIGNMENT_DEL: `DELETE FROM assignment WHERE assignment_id = ?`,
-    ASSIGNMENT_UPD: `UPDATE assignment set task_id = $task_id, link = $link, message = $message, startDate = $startDate, endDate = $endDate 
+    ASSIGNMENT_UPD: `UPDATE assignment SET task_id = $task_id, link = $link, message = $message, startDate = $startDate, endDate = $endDate 
       WHERE assignment_id = ?`,
-    ASSIGNMENT_SEL_ALL: `SELECT * FROM assignment`,
-    ASSIGNMENT_SEL: `SELECT * FROM assignment where assignment_id = ?`
+    ASSIGNMENT_SEL: `SELECT * FROM assignment WHERE assignment_id = ?`,
+    ASSIGNMENT_SEL_ALL: `SELECT * FROM assignment WHERE endDate < ?`,
   },
   // GROUPS
   {
@@ -42,21 +54,22 @@ const queryList = [
   },
   // TASKS
   {
-    TASK_INS: `INSERT INTO tasks (link, type, name, isRepeatable) VALUES
-    ($link, $type, $name, $isRepeatable)`,
+    TASK_INS: `INSERT INTO tasks (link, type, name, key_range, isRepeatable) VALUES
+    ($link, $type, $name, $key_range, $isRepeatable)`,
     TASK_DEL: `DELETE FROM tasks WHERE task_id = ?`,
 
-    TASK_PROPERTIES_INS:`INSERT INTO task_requirements (task_id, skill_id, key_range) VALUES 
-    ($task_id, $skill_id, $key_range)`,
+    TASK_PROPERTIES_INS:`INSERT INTO task_requirements (task_id, skill_id) VALUES 
+    ($task_id, $skill_id)`,
+    TASK_PROPERTIES_DEL:`DELETE FROM task_requirements WHERE task_id = ? AND skill_id = ?`
   },
   // PROJECTS
   {
-      PROJECT_INS: `INSERT INTO projects (project_name) VALUES ($project_name)`,
-      PROJECT_DEL: `DELETE FROM projects WHERE project_id = ?`,
-      PROJECT_TASK_INS: `INSERT INTO projects_tasks (project_id, task_id) VALUES ($project_id, $task_id)`,
-      PROJECT_TASK_DEL: `DELETE FROM projects_task WHERE task_id = ? AND project_id = ?`,
-      PROJECT_COMPETENCIES_INS: `INSERT INTO project_competencies (project_id, skill_id) VALUES ($project_id, $skill_id)`,
-      PROJECT_COMPETENCIES_DEL: `DELETE FROM project_competencies WHERE project_id = ? AND skill_id = ?`,
+    PROJECT_INS: `INSERT INTO projects (project_name) VALUES ($project_name)`,
+    PROJECT_DEL: `DELETE FROM projects WHERE project_id = ?`,
+    PROJECT_TASK_INS: `INSERT INTO projects_tasks (project_id, task_id) VALUES ($project_id, $task_id)`,
+    PROJECT_TASK_DEL: `DELETE FROM projects_task WHERE task_id = ? AND project_id = ?`,
+    PROJECT_COMPETENCIES_INS: `INSERT INTO project_competencies (project_id, skill_id) VALUES ($project_id, $skill_id)`,
+    PROJECT_COMPETENCIES_DEL: `DELETE FROM project_competencies WHERE project_id = ? AND skill_id = ?`,
   },
   // SKILLS
   {
